66ce424be5e0aee9b192959f6da37144d8d3e616
Nix-code <nishantbanjade07@gmail.com>
2022-11-13 12:11:28 +0530

    add script that finds date and user information

---
+++ dateanduser.sh
@@ -0,0 +1,15 @@
+# this is comment and it won't get executed
+# below line is the special comment followed by ! which says the shell
+# script starts from this line
+
+#!/bin/bash 
+# This script displays the date and who's logged on
+
+echo -n  The time and date are:
+date
+
+echo "Let's see who's logged into the system"
+who
+
+
+
